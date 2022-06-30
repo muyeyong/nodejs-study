7fbffddd4be58f283292ea0359c8ad6ed79e124c
muyeyong <xuyongshuaige@gmail.com>
2022-06-30 17:32:31 +0800

    全双工

--- day07/duplex/client.js
+++ day07/duplex/client.js
@@ -1,7 +1,7 @@
 /* 
 全双工通信存在的问题
   时序问题： 请求顺序和返回顺序不一定一一对应 --> 使用包序号
-  包不完整：太长了，可以分隔 --> 使用包头记录body的长度
+  包不完整(粘包、缺包)：太长了，可以分隔 --> 使用包头记录body的长度
  */
 const prorobuffs = require('protocol-buffers')
 const fs = require('fs')
@@ -19,19 +19,30 @@ const getRandomid = () => {
 
 const encode = () => {
   const id = getRandomid()
-
+  console.log('client encode', seq, id)
   const requestBody = {
     id
   }
-  const requestHeader = {
-    bodyLength: message.RequestBody.encode(requestBody).length,
-    seq: seq++
-  }
+  const body = message.RequestBody.encode(requestBody)
+  const header = Buffer.alloc(6)
+  header.writeInt16BE(seq++)
+  header.writeInt32BE(body.length, 2)
+ return Buffer.concat([header, body])
+}
 
- return  message.Request.encode({
-    header: requestHeader,
-    body: requestBody
-  })
+const decode = (buffer) => {
+  const header = buffer.slice(0, 6)
+  const seq = header.readInt16BE()
+  const { name } = message.ResponseBody.decode(buffer.slice(6))
+  return { seq, name}
+}
+
+const checkComplete = (buffer) => {
+  if (buffer.length < 6) {
+    return 0
+  }
+  const bodyLength = buffer.readInt32BE(2)
+  return 6 + bodyLength
 }
 
 socket.connect({
@@ -43,10 +54,20 @@ socket.connect({
 //   socket.write(encode())
 // }, 500)
 
-socket.write(encode())
+for(let i = 0;i < 10; i++) {
+  socket.write(encode())
+}
+
 
 socket.on('data', (buffer) => {
-  console.log('client', message.Response.decode(buffer)) 
+  let oldBuffer = null
+  let packageLength = 0
+  while(packageLength = checkComplete(buffer)) {
+    const package = buffer.slice(0, packageLength)
+    buffer = buffer.slice(packageLength)
+    console.log('client', decode(package)) 
+  }
+  oldBuffer = buffer
   // const id = getRandomid()
   // console.log('client send:', id)
   // socket.write(message.Request.encode({ id }))
--- day07/duplex/server.js
+++ day07/duplex/server.js
@@ -16,26 +16,44 @@ const encode = (id, seq) => {
   const responseBody = {
     name: menus[id]
   }
-  const responseHeader = {
-    seq,
-    bodyLength: message.ResponseBody.encode(responseBody).length
-  }
 
-  return message.Response.encode({
-    header: responseHeader,
-    body: responseBody
-  })
- 
+  const body = message.ResponseBody.encode(responseBody)
+
+  const header = Buffer.alloc(6)
+  header.writeInt16BE(seq)
+  header.writeInt32BE(body.length, 2)
+  return Buffer.concat([header, body])
 }
 
+const decode = (buffer) => {
+  const header = buffer.slice(0, 6)
+  const seq = header.readInt16BE()
+  const { id } = message.RequestBody.decode(buffer.slice(6))
+  return { seq, id}
+}
+
+
+const checkComplete = (buffer) => {
+  if (buffer.length < 6) {
+    return 0
+  }
+  const bodyLength = buffer.readInt32BE(2)
+  return 6 + bodyLength
+}
 const server = net.createServer((socket) => {
+  let oldBuffer = null
   socket.on('data', buffer => {
-    // console.log('server:', buffer,  message.Request.decode(buffer))
-    console.log(message.Request.decode(buffer))
-    const { header: { seq, bodyLength}, body: { id } } = message.Request.decode(buffer)
-    setTimeout(() => {
-      socket.write(encode(id, seq))
-    }, 100 + (Math.random() * 1000))
+    if (oldBuffer) {
+      buffer = Buffer.concat([oldBuffer, buffer])
+    }
+    let packageLength = 0
+    while(packageLength = checkComplete(buffer)) {
+      const package = buffer.slice(0, packageLength)
+      buffer = buffer.slice(packageLength)
+      const result = decode(package)
+      socket.write(encode(result.id, result.seq))
+    }
+    oldBuffer = buffer
   })
 })
 
